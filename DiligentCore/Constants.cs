77ea3e0ab097db7bc10df430b894687bc7bff7cc
Ruben Gomes <rbnpontes@gmail.com>
2024-11-05 18:25:07 -0300

    feat: add diligent version to Constants.cs

--- DiligentCore/Constants.cs
+++ DiligentCore/Constants.cs
@@ -1,6 +1,7 @@
 namespace Diligent;
 
-public static class Constants
+public static partial class Constants
 {
     public const string LibName = "runtimes/win-x64/native/DiligentCoreLib.dll";
+    public const int DiligentApiVersion = 255001;
 }
