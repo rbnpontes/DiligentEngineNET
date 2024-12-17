6f77322037a7658c9a1aa5174b1f2809052d138b
Ruben Gomes <rbnpontes@gmail.com>
2024-12-17 16:47:10 -0300

    feat: add openxr support

--- DiligentCore/Implementations/EngineFactoryVkImpl.cs
+++ DiligentCore/Implementations/EngineFactoryVkImpl.cs
@@ -23,8 +23,12 @@ internal partial class EngineFactoryVk : IEngineFactoryVk
     {
         var numDeferredContexts = GetNumDeferredContexts(createInfo);
         var createInfoData = EngineVkCreateInfo.GetInternalStruct(createInfo);
+        var openXrAttribsData = OpenXRAttribs.GetInternalStruct(createInfo.XRAttribs ?? new OpenXRAttribs());
         var renderDevicePtr = IntPtr.Zero;
         var deviceContexts = new IntPtr[numDeferredContexts];
+
+        if (createInfo.XRAttribs is not null)
+            createInfoData.pXRAttribs = new IntPtr(&openXrAttribsData);
         
         fixed(void* deviceContextsPtr = deviceContexts.AsSpan())
             Interop.engine_factory_vk_create_device_and_contexts_vk(
