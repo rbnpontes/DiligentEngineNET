d57159de186d1a60f41c79de0d79b3d66c059cde
Ruben Gomes <rbnpontes@gmail.com>
2024-12-18 09:55:25 -0300

    feat: add graphics pipeline state tests

---
+++ DiligentCore.Tests/GraphicsPipelineStateTest.cs
@@ -0,0 +1,32 @@
+namespace Diligent.Tests;
+
+[TestFixture]
+public class GraphicsPipelineStateTest : BasePipelineStateTest
+{
+    [Test]
+    public void MustCreate()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        Assert.That(pipeline, Is.Not.Null);
+    }
+
+    [Test]
+    public void MustCreateShaderResourceBinding()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        using var srb = pipeline.CreateShaderResourceBinding();
+        Assert.Multiple(() =>
+        {
+            Assert.That(pipeline, Is.Not.Null);
+            Assert.That(srb, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    public void MustGetDesc()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        var desc = pipeline.Desc;
+        Assert.That(desc.Name, Is.EqualTo("Graphics Pipeline"));
+    }
+}
