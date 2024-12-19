6f5756125049959bf7c30fc72b7d216442d1f0f5
Ruben Gomes <rbnpontes@gmail.com>
2024-12-19 08:20:05 -0300

    feat: add new graphics pipeline state unit test

--- DiligentCore.Tests/GraphicsPipelineStateTest.cs
+++ DiligentCore.Tests/GraphicsPipelineStateTest.cs
@@ -30,4 +30,43 @@ public class GraphicsPipelineStateTest : BasePipelineStateTest
         var desc = pipeline.Desc;
         Assert.That(desc.Name, Is.EqualTo("Graphics Pipeline"));
     }
+
+    [Test]
+    public void MustGetStaticVariableCount()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        Assert.Multiple(() =>
+        {
+            Assert.That(pipeline.GetStaticVariableCount(ShaderType.Vertex), Is.GreaterThan(0));
+            Assert.That(pipeline.GetStaticVariableCount(ShaderType.Pixel), Is.GreaterThan(0));
+        });
+    }
+
+    [Test]
+    public void MustGetVariableByName()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        var vertexVar = pipeline.GetStaticVariableByName(ShaderType.Vertex, "GlobalConstants");
+        var pixelVar = pipeline.GetStaticVariableByName(ShaderType.Pixel, "GlobalConstants");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(vertexVar, Is.Not.Null);
+            Assert.That(pixelVar, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    public void MustGetStaticVariableByIndex()
+    {
+        using var pipeline = CreateGraphicsPipelineState();
+        var vertexVar = pipeline.GetStaticVariableByIndex(ShaderType.Vertex, 0);
+        var pixelVar = pipeline.GetStaticVariableByIndex(ShaderType.Pixel, 0);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(vertexVar, Is.Not.Null);
+            Assert.That(pixelVar, Is.Not.Null);
+        });
+    }
 }
